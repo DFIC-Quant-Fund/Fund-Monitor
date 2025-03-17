7679284ba65f522f354ae9c05679a3936f68794a
Raj Pandya <rajpandya737@gmail.com>
2025-03-16 23:21:22 -0400

    add market comparison file

--- PerformanceTracker.py
+++ PerformanceTracker.py
@@ -2,7 +2,13 @@ import os
 import pandas as pd
 import sys
 import yfinance as yf
-from performance import DataProcessor, Benchmark, PortfolioPerformance, RiskMetrics
+from performance import (
+    DataProcessor, 
+    Benchmark, 
+    PortfolioPerformance, 
+    RiskMetrics, 
+    MarketComparison
+)
 
 
 # def aggregate_data_old(input_file, output_file):
@@ -57,7 +63,7 @@ class Ratios:
         return daily_sortino_ratio, annualized_sortino_ratio
     
     def treynor_ratio(self, risk_free_return):
-        market_comparison = MarketComparison()
+        market_comparison = MarketComparison(output_folder)
         return market_comparison.portfolio_risk_premium(risk_free_return) / market_comparison.beta()
 
     def information_ratio(self, benchmark='custom'):
@@ -77,50 +83,6 @@ class Ratios:
 
         return information_ratio
 
-class MarketComparison:
-    def __init__(self):
-        pass
-
-    def beta(self, benchmark='custom'):
-        benchmark_class = Benchmark(output_folder)
-        if benchmark == 'custom':
-            benchmark_df = pd.read_csv(os.path.join(output_folder, 'custom_benchmark.csv'))
-        else:
-            benchmark_df = benchmark_class.get_spy_benchmark()
-        
-        df = pd.read_csv(os.path.join(output_folder, 'portfolio_total.csv'))
-        daily_portfolio_return = df['pct_change'].dropna()
-        daily_benchmark_var, _ = benchmark_class.benchmark_variance(benchmark)
-        covariance = daily_portfolio_return.cov(benchmark_df['pct_change'])
-        beta = covariance / daily_benchmark_var
-
-        return beta
-        
-
-    def alpha(self, risk_free_rate, benchmark='custom'): 
-        benchmark_class = Benchmark(output_folder)
-        annual_benchmark_return = benchmark_class.benchmark_average_return(benchmark)[1]
-        portfolio_performance = PortfolioPerformance(output_folder)
-        print("benchmark: ", benchmark)
-        print("annual_benchmark_return ", annual_benchmark_return)
-        alpha = (portfolio_performance.annualized_average_return() - risk_free_rate) - self.beta() * (annual_benchmark_return - risk_free_rate)
-
-        return alpha
-        
-    def portfolio_risk_premium(self, risk_free_return):
-        portfolio_performance = PortfolioPerformance(output_folder)
-        return portfolio_performance.annualized_average_return() - risk_free_return
-
-    def risk_adjusted_return(self, risk_free_return):
-        risk_metrics = RiskMetrics(output_folder)
-        benchmark = Benchmark(output_folder)
-        benchmark_vol = benchmark.benchmark_volatility()[1]
-        portfolio_volatility = risk_metrics.annualized_volatility()
-        portfolio_risk_prem = self.portfolio_risk_premium(risk_free_return)
-        risk_adjusted_return = portfolio_risk_prem * benchmark_vol / portfolio_volatility + risk_free_return
-        
-        return risk_adjusted_return
-
 
 
 def main():
@@ -135,8 +97,8 @@ def main():
     benchmark = Benchmark(output_folder)
     portfolio_performance = PortfolioPerformance(output_folder)
     risk_metrics = RiskMetrics(output_folder)
+    market_comparison = MarketComparison(output_folder)
     ratios = Ratios()
-    market_comparison = MarketComparison()
 
     # run these once only after running portfolio.py once
     data_processor.aggregate_data(market_values_file, cash_file, dividend_file, output_file)
--- performance/__init__.py
+++ performance/__init__.py
@@ -2,6 +2,7 @@ from .DataProcessor import DataProcessor
 from .Benchmark import Benchmark
 from .PortfolioPerformance import PortfolioPerformance
 from .RiskMetrics import RiskMetrics
+from .MarketComparison import MarketComparison
 
 # this is what gets imported when you just import all of performance
-__all__ = ["DataProcessor", "Benchmark", "PortfolioPerformance", "RiskMetrics"]
+__all__ = ["DataProcessor", "Benchmark", "PortfolioPerformance", "RiskMetrics", "MarketComparison"]
